57fa0157cdda585174771e8405325afbe9a53169
Tan Phat Nguyen <53106182+malderfox30@users.noreply.github.com>
2022-08-11 23:35:28 +0700

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "7MgXxpdUHaGn3_BWZuNNI8KmU8q42XO2";
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const GOERLI_PRIVATE_KEY = "544e337bc33a9ea8f284fb107d054231025bcaacade859c9ffafe64b856837bb";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
+};
+
+export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const EVAToken = await ethers.getContractFactory("EVAToken");
+  const token = await EVAToken.deploy();
+
+  console.log("Token address:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
