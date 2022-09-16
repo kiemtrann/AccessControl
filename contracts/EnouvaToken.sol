2d4573bb11ca16684708184a5d34de244ebde09c
Tan Phat Nguyen <53106182+malderfox30@users.noreply.github.com>
2022-09-16 14:00:06 +0700

    chore(main): added eslintrc, commands

--- contracts/EnouvaToken.sol
+++ contracts/EnouvaToken.sol
@@ -3,25 +3,34 @@ pragma solidity ^0.8.9;
 
 interface ERC20Interface {
     function totalSupply() external view returns (uint256);
+
     function balanceOf(address account) external view returns (uint256);
+
     function allowance(address owner, address spender) external view returns (uint256);
+
     function transfer(address to, uint256 amount) external returns (bool);
+
     function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address from, address to, uint256 amount) external returns (bool);
- 
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
- 
+
 contract EVAToken is ERC20Interface {
     string public symbol;
     string public name;
     uint8 public decimals;
     uint256 public _totalSupply;
- 
-    mapping(address => uint) balances;
+
+    mapping(address => uint256) balances;
     mapping(address => mapping(address => uint256)) allowed;
- 
+
     constructor() {
         symbol = "EVA";
         name = "Enouva";
@@ -30,37 +39,41 @@ contract EVAToken is ERC20Interface {
         balances[msg.sender] = _totalSupply;
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
- 
-    function totalSupply() override public view returns (uint256) {
+
+    function totalSupply() public view override returns (uint256) {
         return _totalSupply;
     }
- 
-    function balanceOf(address tokenOwner) override public view returns (uint256) {
+
+    function balanceOf(address tokenOwner) public view override returns (uint256) {
         return balances[tokenOwner];
     }
- 
-    function transfer(address to, uint256 amount) override public returns (bool) {
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
         balances[msg.sender] = balances[msg.sender] - amount;
         balances[to] = balances[to] + amount;
         emit Transfer(msg.sender, to, amount);
         return true;
     }
- 
-    function approve(address spender, uint256 amount) override public returns (bool) {
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
         allowed[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
- 
-    function transferFrom(address from, address to, uint256 amount) override public returns (bool) {
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
         balances[from] = balances[from] - amount;
         allowed[from][msg.sender] = allowed[from][msg.sender] - amount;
         balances[to] = balances[to] + amount;
         emit Transfer(from, to, amount);
         return true;
     }
- 
-    function allowance(address tokenOwner, address spender) override public view returns (uint256) {
+
+    function allowance(address tokenOwner, address spender) public view override returns (uint256) {
         return allowed[tokenOwner][spender];
     }
 }
---
+++ test/EnouvaToken.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+
+describe("EnouvaToken", function () {
+  describe("Deployment", function () {
+    it("Should set the right unlockTime", async function () {
+      expect(1 + 1).to.equal(2);
+    });
+  });
+});
